a79f9e0caccc97271e2720d601dc0567611743f2
inoha_naito <inoha925@yahoo.co.jp>
2022-01-17 23:05:55 +0900

    [update] アップロード機能の実装

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { ref, uploadBytes } from 'firebase/storage';
+
+import { storage } from '../scripts/firebase'; 
+
+const onFileInputChange = e => {
+  const file = e.target.files[0];
+  const storageRef = ref(storage, file.name);
+  uploadBytes(storageRef, file).then(() => {
+    console.log('Uploaded!');
+  });
+}
 
 const Upload = () => {
   return (
     <React.Fragment>
-      <p>Hello, React!</p>
+      <input type='file' onChange={onFileInputChange} />
     </React.Fragment>
   );
 }
