ba989260306587029b9ef05059d7c59feb82cc5a
inoha_naito <inoha925@yahoo.co.jp>
2022-01-18 23:36:13 +0900

    [update] ボタンで送信する機能の実装

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ref, uploadBytes } from 'firebase/storage';
 
 import { storage } from '../scripts/firebase'; 
 
-const onFileInputChange = e => {
-  const files = Array.from(e.target.files);
-  files.forEach((file) => {
-    const storageRef = ref(storage, 'images/' + file.name);
-    uploadBytes(storageRef, file).then(() => {
-      console.log('Uploaded!');
-    });
-  });
-}
-
 const Upload = () => {
+  const [files, setFiles] = useState([]);
+
+  const onFileInputChange = e => {
+    setFiles(Array.from(e.target.files));
+  }
+
+  const clickSubmit = e => {
+    e.preventDefault();
+    files.forEach((file) => {
+      const storageRef = ref(storage, `images/${file.name}`);
+      uploadBytes(storageRef, file).then(() => {
+        console.log('Uploaded!');
+      });
+    });
+  }
+
   return (
     <React.Fragment>
-      <input
-        type='file'
-        accept='image/*'
-        multiple
-        onChange={onFileInputChange}
-      />
+      <form>
+        <input
+          type='file'
+          accept='image/*'
+          multiple
+          onChange={onFileInputChange}
+        />
+        <button type='submit' onClick={clickSubmit}>送信</button>
+      </form>
     </React.Fragment>
   );
 }
