7a2595e8e734ff99af51607a2dfdada354fbb35b
inoha_naito <inoha925@yahoo.co.jp>
2022-01-18 23:08:08 +0900

    [fix] 複数ファイルを一度に送信できるように修正

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -4,17 +4,24 @@ import { ref, uploadBytes } from 'firebase/storage';
 import { storage } from '../scripts/firebase'; 
 
 const onFileInputChange = e => {
-  const file = e.target.files[0];
-  const storageRef = ref(storage, 'images/' + file.name);
-  uploadBytes(storageRef, file).then(() => {
-    console.log('Uploaded!');
+  const files = Array.from(e.target.files);
+  files.forEach((file) => {
+    const storageRef = ref(storage, 'images/' + file.name);
+    uploadBytes(storageRef, file).then(() => {
+      console.log('Uploaded!');
+    });
   });
 }
 
 const Upload = () => {
   return (
     <React.Fragment>
-      <input type='file' accept='image/*' onChange={onFileInputChange} />
+      <input
+        type='file'
+        accept='image/*'
+        multiple
+        onChange={onFileInputChange}
+      />
     </React.Fragment>
   );
 }
